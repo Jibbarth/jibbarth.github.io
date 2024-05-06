5eb8d3c2b76adca90eb0ad179ec941333029bbc5
Jibbarth <barth.jib@gmail.com>
2024-05-06 14:41:37 +0000

    Deploying to master from  @ f422a1b3a6863176e53addc3c4352ff247bf44ca 🚀

--- assets/controllers/tweet_embed_controller-45487951ccb866008ddfd6f7ce7bed08.js
+++ assets/controllers/tweet_embed_controller-45487951ccb866008ddfd6f7ce7bed08.js
@@ -9,12 +9,26 @@ export default class extends Controller {
     }
 
     connect() {
-        if (document.readyState === "complete") {
-            this._loadTweet();
-             return;
-        }
+        // Load Twitter SDK
+        // @see https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
+        window.twttr = (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                t = window.twttr || {};
+            if (d.getElementById(id)) return t;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
 
-        window.addEventListener('load', () => this._loadTweet());
+            t._e = [];
+            t.ready = function(f) {
+                t._e.push(f);
+            };
+
+            return t;
+        }(document, "script", "twitter-wjs"));
+
+        window.twttr.ready(() => this._loadTweet());
     }
 
     _loadTweet() {
