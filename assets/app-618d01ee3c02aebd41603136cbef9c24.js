7a15315ed31dde8f5c1252a595aacd749d3b6ec7
Jibbarth <barth.jib@gmail.com>
2024-10-05 12:30:10 +0000

    Deploying to master from  @ 0a5251df918960b06ba8871a8c44279eb7cb7d21 🚀

--- assets/app-618d01ee3c02aebd41603136cbef9c24.js
+++ assets/app-618d01ee3c02aebd41603136cbef9c24.js
@@ -7,12 +7,8 @@
 import './css/app.css';
 import './bootstrap.js';
 
-import twemoji from 'twemoji';
-
 let current_url = null;
 document.addEventListener("turbo:load", (evt) => {
-    twemoji.parse(document.body);
-
     window.umami.track(props => ({ ...props,
         'referrer': current_url !== null ? current_url : document.referrer,
         'title': document.getElementsByTagName('title')[0].innerText,
