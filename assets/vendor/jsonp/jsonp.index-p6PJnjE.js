0b6a5918766b8953f91b0051c2d0a57bdf802077
Jibbarth <Jibbarth@users.noreply.github.com>
2025-05-30 18:30:57 +0000

    Deploying to master from  @ 6a646c0cb192806774245cde91e258d5b00bcc0d 🚀

--- assets/vendor/jsonp/jsonp.index-p6PJnjE.js
+++ assets/vendor/jsonp/jsonp.index-p6PJnjE.js
@@ -1,5 +1,5 @@
 /**
- * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
+ * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
  * Original file: /npm/jsonp@0.2.1/index.js
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
