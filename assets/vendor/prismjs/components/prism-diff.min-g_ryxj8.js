ef7fb92a796efa704dbd7170426f00d12434d448
Jibbarth <Jibbarth@users.noreply.github.com>
2026-07-09 14:53:43 +0000

    Deploying to master from  @ 78c8a1d914a6e22624b56742c960f0551175be47 🚀

--- assets/vendor/prismjs/components/prism-diff.min-g_ryxj8.js
+++
@@ -1,9 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v4.62.2 and esbuild v0.28.1.
- * Original file: /npm/prismjs@1.29.0/components/prism-diff.min.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-(function(n){n.languages.diff={coord:[/^(?:\*{3}|-{3}|\+{3}).*$/m,/^@@.*@@$/m,/^\d.*$/m]};var i={"deleted-sign":"-","deleted-arrow":"<","inserted-sign":"+","inserted-arrow":">",unchanged:" ",diff:"!"};Object.keys(i).forEach((function(e){var d=i[e],r=[];/^\w+$/.test(e)||r.push(/\w+/.exec(e)[0]),e==="diff"&&r.push("bold"),n.languages.diff[e]={pattern:RegExp("^(?:["+d+`].*(?:\r
-?|
-|(?![\\s\\S])))+`,"m"),alias:r,inside:{line:{pattern:/(.)(?=[\s\S]).*(?:\r\n?|\n)?/,lookbehind:!0},prefix:{pattern:/[\s\S]/,alias:/\w+/.exec(e)[0]}}}})),Object.defineProperty(n.languages.diff,"PREFIXES",{value:i})})(Prism);
